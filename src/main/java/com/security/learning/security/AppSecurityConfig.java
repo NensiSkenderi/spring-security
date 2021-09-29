7ddffc64809c66513d3421db4fccefe3e1b97e30
Nensi Skenderi <nensiskenderi20@gmail.com>
2021-09-29 12:24:24 +0200

    use permission based auth on method level

--- src/main/java/com/security/learning/security/AppSecurityConfig.java
+++ src/main/java/com/security/learning/security/AppSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +19,7 @@ import static com.security.learning.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) // to use preauthorize annotation on method level
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -28,13 +30,13 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
+                //THE ORDER THAT WE DEFINE MATCHES MATTERS, it goes thru the matchers one by one
                 .antMatchers("/", "index", "/css/", "/js").permitAll()
                 .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
-                //IMPLEMENT PERMISSION BASED AUTH
-                .antMatchers(HttpMethod.GET, "/management/**").hasAnyRole(ADMIN.name(), TRAINEE.name())
-                .antMatchers(HttpMethod.POST, "/management/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.PUT, "/management/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.DELETE, "/management/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET, "/management/**").hasAnyRole(ADMIN.name(), TRAINEE.name())
+//                .antMatchers(HttpMethod.POST, "/management/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT, "/management/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.DELETE, "/management/**").hasAuthority(COURSE_WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
--- src/main/java/com/security/learning/student/StudentManagementController.java
+++ src/main/java/com/security/learning/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.security.learning.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -15,22 +16,31 @@ public class StudentManagementController {
             new Student(3, "Nensi3")
     );
 
+    /*
+    inside @PreAuthorize("") we can put hasRole(), hasAnyRole(), hasAuthority(), hasAnyAuthority()
+     */
+
+    // use permission based auth on method level
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_TRAINEE')")
     public List<Student> getAllStudents(){
         return STUDENTS_LIST;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void addNewStudent(@RequestBody Student student){
         System.out.println(student);
     }
 
     @DeleteMapping("{id}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("id") Integer studentId){
         System.out.println(studentId);
     }
 
     @PutMapping("{id}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("id") Integer studentId, @RequestBody Student student){
         System.out.println(String.format("%s %s", studentId, student));
     }
